333aec91ff5ed29cbdf494756c181b5144280fce
root <root@dsaid.gov.sg>
2020-05-26 01:36:04 +0000

    added monitoring window as argument

--- sysdig-monitoring.sh
+++ sysdig-monitoring.sh
@@ -11,11 +11,12 @@ echo "Logging-date: ${date}"
 outputFileName=${date}_logs
 
 containerName=$1
-echo "Container to monitor: ${containerName}"
+attachment=${2}${date}_logs
+email=$3
+window=$4
+echo "Container to monitor: ${containerName} with window: ${window} seconds"
 
-
-
-sysdig -pc -M 60 -w ${date}.scap -c spy_users container.name=${containerName} >> ${PWD}/logs/${outputFileName}
+sysdig -pc -M ${window} -w ${date}.scap -c spy_users container.name=${containerName} >> ${PWD}/logs/${outputFileName}
 
 echo "${date}_Logging-complete"
 echo "Removing scap to clear space"
@@ -24,9 +25,6 @@ rm ${date}.scap
 # Send Email
 echo "Sending mail"
 
-attachment=${2}${date}_logs
-email=$3
-
 echo "Recipient: ${email} --------------- Attaching: ${attachment}"
 
 echo "Logs for ${date} ----- Please check " | mutt -a ${attachment} -s "Sysdig logging ${date}" -- ${email}
@@ -34,3 +32,4 @@ echo "Logs for ${date} ----- Please check " | mutt -a ${attachment} -s "Sysdig l
 echo "Mail sent"
 
 exit 0
+
