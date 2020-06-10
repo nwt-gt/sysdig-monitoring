d082c6b16bfa23b3a090dcb013f51e1f8cb11bf6
root <root@dsaid.gov.sg>
2020-06-10 22:38:45 +0800

    added log filtering script and modified sysdig-monitoring

--- sysdig-monitoring.sh
+++ sysdig-monitoring.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
 
+# redirect sysdig-monitoring stdout/stderr to a sysdig_logs
+exec > >(tee -a logs/sysdig_logs) 2>&1
+
 # Monitoring container
-echo "Sysdig logging"
+echo "***Sysdig logging***"
 
 set -eu
 
-if [[ $# -ne 4 ]]; then
+# takes in 2 arg. containerName & monitoring window
+if [[ $# -ne 2 ]]; then
     echo "Wrong arguments. Call this script with:"
-    echo "${0} <containerName> <pathToAttachment> <Email> <Window>"
+    echo "${0} <containerName> <Window>"
     exit 1;
 fi
 
 
 date=`date +%d-%m-%Y`
-echo "Logging-date: ${date}"
+dateTime=`date +%d-%m-%Y:%H:%M:%S`
+
+echo "Logging-date --- ${dateTime}"
+
 outputFileName=${date}_logs
-
 containerName=$1
-attachment=${2}${outputFileName}
-email=$3
-window=$4
-echo "Container to monitor: ${containerName} with window: ${window} seconds"
+window=$2
 
-sysdig -pc -M ${window} -w ${date}.scap -c spy_users container.name=${containerName} >> ${attachment}
+echo "Container to monitor --- ${containerName}" && echo "Window --- ${window} seconds"
 
-echo "${date}_Logging-complete"
-echo "Removing scap to clear space"
-rm ${date}.scap
+# runs command at background and stream scap output to logs directly
+nohup stdbuf -oL -eL sysdig -pc -G ${window} -W 2 -w scap/test.scap -c spy_users container.name=${containerName} >> logs/${outputFileName} 2>&1 &
 
-# Send Email
-echo "Sending mail"
-
-echo "Recipient: ${email} --------------- Attaching: ${attachment}"
-
-echo "Logs for ${date} ----- Please check " | mutt -a ${attachment} -s "Sysdig logging ${date}" -- ${email}
-
-echo "Mail sent"
-
-exit $?
-
+exit 0
