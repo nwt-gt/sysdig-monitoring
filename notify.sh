1507bdb4de983af0323f9d3af076c11472b05486
root <root@dsaid.gov.sg>
2020-06-12 11:08:23 +0800

    specified filename

--- notify.sh
+++ notify.sh
@@ -6,10 +6,10 @@ exec > >(tee -a logs/notify_logs) 2>&1
 filePath=/home/nwt/swgapp/sysdig/logs/filter/*_logs
 
 # moniters modification to the filePath and outputs path
-inotifywait -m --event modify --format '%w' ${filePath} | while read f; do \
-        #Able to do call. Eg Send email
-        echo "$f"; \
-        
+inotifywait -m --event modify --format '%w' ${filePath} | while read f; do
+        #Able to trigger action. Eg Send email
+        echo "Full path - ${f}"
+        echo "File Name - ${f##*/}"; 
     done
 
 exit 0 
