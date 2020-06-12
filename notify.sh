241518f2bc06a0a3e828ba7ecdfcafb255704667
root <root@dsaid.gov.sg>
2020-06-12 11:49:06 +0800

    allows monitorying of dynamic path

--- notify.sh
+++ notify.sh
@@ -3,13 +3,21 @@
 # redirect notify stdout/stderr to a notify_logs
 exec > >(tee -a logs/notify_logs) 2>&1
 
-filePath=/home/nwt/swgapp/sysdig/logs/filter/*_logs
+set -eu
+
+if [[ $# -ne 1 ]]; then
+    echo "Please enter the file path"
+    exit 1
+fi
+
+filePath=$1
 
 # moniters modification to the filePath and outputs path
 inotifywait -m --event modify --format '%w' ${filePath} | while read f; do
-        #Able to trigger action. Eg Send email
+        # Able to trigger action. Eg Send email
+        # Here I sysout the file details and append to trigger
         echo "Full path - ${f}"
         echo "File Name - ${f##*/}"; 
-    done
+    done >> logs/trigger 2>&1 &
 
 exit 0 
